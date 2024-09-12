29923dc12e7fd10fb944ac30e1c2b3783bcc22d6
Jonty Kikov <johnkikov01@gmail.com>
2024-09-12 03:14:50 +0300

    upd

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { SiDiscord, SiGithub, SiX } from 'react-icons/si'
+import { SiDiscord, SiGithub, SiX, SiTwitter } from 'react-icons/si'
 import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
@@ -11,27 +11,10 @@ const Footer: React.FC = () => {
           variant={'ghost'}
           size={'icon'}
           className="text-muted-foreground/50"
+          disabled // Added to make the button inactive
         >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://x.com/morphic_ai" target="_blank">
-            <SiX size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://git.new/morphic" target="_blank">
-            <SiGithub size={18} />
+          <Link href="https://twitter.com" target="_blank">
+            <SiTwitter size={18} />
           </Link>
         </Button>
       </div>
