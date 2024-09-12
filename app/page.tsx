eb1cd40444764fa938db390faf5df815392c046f
Jonty Kikov <johnkikov01@gmail.com>
2024-09-12 03:26:00 +0300

    added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import { Chat } from '@/components/chat'
 import { generateId } from 'ai'
 import { AI } from './actions'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const maxDuration = 60
 
 export default function Page() {
   const id = generateId()
   return (
-    <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Chat id={id} />
-    </AI>
+    <>
+      <Analytics />  
+      <SpeedInsights />  
+      <AI initialAIState={{ chatId: id, messages: [] }}>
+        <Chat id={id} />
+      </AI>
+    </>
   )
 }
